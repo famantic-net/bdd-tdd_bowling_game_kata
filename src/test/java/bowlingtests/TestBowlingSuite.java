1a57519dcf836bba80a3cb8185633e921b510cc0
Nenad Antic <nenad.antic@bisnode.com>
2020-01-22 12:27:38 +0100

    Removed all code

--- src/test/java/bowlingtests/TestBowlingSuite.java
+++ src/test/java/bowlingtests/TestBowlingSuite.java
@@ -2,68 +2,7 @@ package bowlingtests;
 
 import bowling.BowlingGame;
 import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import static org.junit.Assert.assertEquals;
 
 public class TestBowlingSuite {
-
-    @Test
-    public void testGutterGame() {
-        BowlingGame game = new BowlingGame();
-        testGutterGame(game);
-    }
-    public void testGutterGame(BowlingGame game) {
-
-        for (int i=0; i<2; i++) {
-            for (int j=0; j<10; j++) {
-                game.roll(0);
-            }
-        }
-        assertEquals(0,game.getScore());
-    }
-
-    @Test
-    public void testSpare() {
-        BowlingGame game = new BowlingGame();
-        rollFrame(game,5,5);
-        rollFrame(game,3,0);
-        for (int j=2; j<10; j++) {
-            rollFrame(game,0,0);
-        }
-        assertEquals(16, game.getScore());
-    }
-
-    @Test
-    public void testStrike() {
-        BowlingGame game = new BowlingGame();
-        rollFrame(game,10);
-        rollFrame(game,10);
-        for (int j=2; j<10; j++) {
-            rollFrame(game,0,0);
-        }
-        assertEquals(30, game.getScore());
-
-    }
-
-    public void rollFrame(BowlingGame game, int... rolls) {
-        for (int ball : rolls) {game.roll(ball);}
-    }
-
-    @Test
-    public void testRandomGame() {
-        BowlingGame game = new BowlingGame();
-        randomGame(game, new ArrayList<Integer>(Arrays.asList(5,5, 3,7, 2,8, 9,1, 4,5, 3,7, 8,2, 9,1, 10, 5,5,10)));
-        assertEquals(164, game.getScore());
-    }
-    public void randomGame(BowlingGame game, List<Integer> rolls){
-        for (int ball : rolls) {
-            game.roll(ball);
-        }
-    }
-
-
 }
--- src/test/java/cucumber/steps/BowlingStepsLambdas.java
+++ src/test/java/cucumber/steps/BowlingStepsLambdas.java
@@ -2,6 +2,8 @@ package cucumber.steps;
 
 import bowling.BowlingGame;
 import bowlingtests.TestBowlingSuite;
+import cucumber.api.DataTable;
+import cucumber.api.PendingException;
 import cucumber.api.java8.En;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -16,31 +18,57 @@ public class BowlingStepsLambdas implements En {
     public BowlingStepsLambdas() {
         TestBowlingSuite suite = new TestBowlingSuite();
 
-        When("^First frame is a spare$", () -> {
-            suite.rollFrame(game,5,5);
+        When("^All balls miss all pins$", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
         });
 
-        When("^Next roll is a (\\d+)$", (Integer pins) -> {
-            suite.rollFrame(game, pins,0);
+        Then("^Game score is zero$", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+
+        When("^First frame is a spare$", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+
+        When("^Next roll is a (\\d+)$", (Integer arg1) -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+
+        When("^The rest from frame (\\d+) are gutterballs$", (Integer arg1) -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+
+        Then("^Game score is (\\d+)$", (Integer arg1) -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
         });
 
         When("^First roll is a srike$", () -> {
-            game.roll(10);
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
         });
 
         When("^Second Roll is a strike$", () -> {
-            game.roll(10);
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
         });
 
-        When("^The rest from frame (\\d+) are gutterballs$", (Integer frame) -> {
-            for (int j=frame-1; j<10; j++) {
-                suite.rollFrame(game,0,0);
-            }
+        When("^Play is$", (DataTable arg1) -> {
+            // Write code here that turns the phrase above into concrete actions
+            // For automatic transformation, change DataTable to one of
+            // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+            // E,K,V must be a scalar (String, Integer, Date, enum etc).
+            // Field names for YourType must match the column names in
+            // your feature file (except for spaces and capitalization).
+            throw new PendingException();
         });
 
-        Then("^Game score is (\\d+)$", (Integer score) -> {
-            assertEquals((int)score, (int)game.getScore());
-        });
+
 
     }
 
